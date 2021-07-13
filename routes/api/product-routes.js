6af32144fedcf98d9ec33a67437907a416ae9bd6
agrosjean <agrosjean007@gmail.com>
2021-07-13 18:18:16 -0400

    Implement product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,19 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  const response = await Product.findAll({ include: [Category, Tag] });
+  res.json(response);
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const response = await Product.findOne({ where: { id: req.params.id }, include: [Category, Tag] });
+  res.json(response);
 });
 
 // create new product
@@ -89,8 +93,10 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  await Product.destroy({ where: { id: req.params.id } });
+  res.json('OK');
 });
 
 module.exports = router;
